8f1b3cd637124638ee80921dc4d18f12b6ff4518
AhmedRoushdy <72147140+zGeNiuSs@users.noreply.github.com>
2025-01-05 02:32:20 +0200

    Create First Project

---
+++ Python Tutorial/Module Assessments/First Module Assessment/first.py
@@ -0,0 +1,4 @@
+# first.py
+print("Hello! My name is Ahmed Roushdy.")
+print("I am 27 years old.")
+print("In this project, I want to learn the basics of Python programming, improve my skills, and explore new techniques")

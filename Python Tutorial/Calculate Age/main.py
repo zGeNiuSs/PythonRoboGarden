503cd961ffd076904f54524f25077d8ab282e5d0
AhmedRoushdy <72147140+zGeNiuSs@users.noreply.github.com>
2025-01-05 03:01:05 +0200

    Create main.py

---
+++ Python Tutorial/Calculate Age/main.py
@@ -0,0 +1,6 @@
+start_year=input("Enter the year of person born/item : ")
+end_year=input("Enter Current year : ")
+
+age= int(end_year)-int(start_year)
+
+print("The age of the person/item is :",age)
